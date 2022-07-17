c158ac90a1c3d3b14a6300d43d3d20009f31b8fa
andvargas <a.vargyas@gmail.com>
2022-07-17 14:55:26 +0100

    removed photo about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import profilePic from '../../images/about-me.jpg';
-import classes from './About.module.css'
+// import profilePic from "../../images/about-me.jpg";
+import classes from "./About.module.css";
 
 const about = (props) => (
-    <div className={classes.container}>
-        <img id={classes.profile} src={profilePic} alt="Kamilla" />
-        <div className={classes.text}>
-            <h3>About Me</h3>
-            <p>I’m a Hungarian-Transylvanian artist, I use a wide variety of mediums, subject matters, techniques and styles. I have been learning from artists who share their skills via youtube and other online platforms. I love experimenting, expressing my thoughts, feelings, ideas, dreams through painting and drawing. I like exploring philosophical topics, symbols, and I usually hide a message behind my compositions.</p>
-        </div>
-        
+  <div className={classes.container}>
+    {/* <img id={classes.profile} src={profilePic} alt="Kamilla" /> */}
+    <div className={classes.text}>
+      <h3>About Me</h3>
+      <p>
+        I’m a Hungarian-Transylvanian artist, I use a wide variety of mediums,
+        subject matters, techniques and styles. I have been learning from
+        artists who share their skills via youtube and other online platforms. I
+        love experimenting, expressing my thoughts, feelings, ideas, dreams
+        through painting and drawing. I like exploring philosophical topics,
+        symbols, and I usually hide a message behind my compositions.
+      </p>
     </div>
-)
+  </div>
+);
 
 export default about;
